53df3c03ff4c4707c6c1745ef0343a8a001f669c
Aaron Kaplan <aaron@iri.columbia.edu>
2021-12-21 12:07:27 -0500

    Theme

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -29,6 +29,7 @@ author = 'IRI'
 # ones.
 extensions = [
     "myst_parser",
+    "sphinxcontrib.fulltoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,9 +46,20 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+html_sidebars = {
+    '**': [
+        'localtoc.html',
+        'searchbox.html',
+    ],
+}
+
+# Suppress index link in navbar, since the index is empty and thus
+# useless. Enable if/when we start adding index entries.
+html_use_index = False
